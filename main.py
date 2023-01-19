4cf7ff692088fa5cf4ce7151cd37b278b3185a32
Jeremy Maslanko <maslankoj@gmail.com>
2023-01-18 20:13:40 -0500

    add html parser

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import youtube as yt
 import database as db
 import random
-
+from bs4 import BeautifulSoup
 
 def random_playlist(con):
     '''Randomly select a channel uploads playlist'''
@@ -31,19 +31,38 @@ def random_video(con, video_id):
     
     return id
 
+def update_html(video_id):
+    url = f"https://www.youtube.com/embed/{video_id}"
+    with open("index.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    tag = soup.iframe
+    tag['src'] = url
+    with open("index.html", "w") as file:
+        file.write(str(soup))
 
-    
+    return print('HTML has been updated with new video!')
+
+#hN3CyODxl-c
+#update_html('hN3CyODxl-c')
 # Need to now create html production file that will get updated with 
 # the new video id.  Will use beautifulsoup to do so    
 
 
 
+# with open("index.html") as fp:
+#     soup = BeautifulSoup(fp, 'html.parser')
+
+
+
+# con = db.init_db('StemVideo.db')
+# db.insert_video_id(con, 'hN3CyODxl-c')
 
 def main():
     con = db.init_db('StemVideo.db')
     playlist = random_playlist(con)
     video = random_video(con, playlist)
     db_id = db.insert_video_id(con, video)
+    update_html(video)
 
 if __name__ == '__main__':
     main()
